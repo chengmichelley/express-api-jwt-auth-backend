ef780d9396221ad139187045f6d915eb5aa1ad92
Michelle Cheng <cheng.michelley@gmail.com>
2026-04-20 23:23:48 -0400

    Auth Backend

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  
+  email:{
+    type: String,
+  },
+
+  phoneNumber: {
+    type: String,
+  },
+
+  address: {
+    type: String,
+  }
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
